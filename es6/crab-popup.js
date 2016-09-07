7c7318635b12cb89125bfc52207602b93ef81b37
邱伟 <406810175@qq.com>
2016-09-07 22:43:04 +0800

    基本功能完成

--- es6/crab-popup.js
+++ es6/crab-popup.js
@@ -11,7 +11,23 @@
                 }
 
                 return ele;
-            }
+            },
+            extends(dest, source) {
+                const args = arguments;
+
+                if (args.length > 2) {
+                    return this.extends.apply(this, this.slice.call(args, 1));
+                } else {
+                    for (const key in source) {
+                        if (source.hasOwnProperty(key)) {
+                            dest[key] = source[key];
+                        }
+                    }
+
+                    return dest;
+                }
+            },
+            slice: [].slice
         };
 
         const data = {
@@ -21,7 +37,11 @@
             setImgs(imgs) {
                 this.imgs = imgs;
             },
-            showImg(index) {
+            setImg(index) {
+                const maxIndex = this.imgs.length - 1;
+                index < 0 && (index = maxIndex);
+                index > maxIndex && (index = 0);
+
                 this.currentImg = this.imgs[index];
                 this.currentIndex = index;
             }
@@ -33,13 +53,34 @@
                     id: 'crab_popup_style',
                     innerText: ''
                 }),
-                    popupBox = document.getElementById('crab_popup_box') || utils.createEle('div', {
+                    popupBox = this.box = document.getElementById('crab_popup_box') || utils.createEle('div', {
                         id: 'crab_popup_box',
-                        innerHTML: '<span class="prev"></span><div id="crab_popup_pane"></div><span class="crab_popup_close"></span><span class="next"></span>'
+                        innerHTML: '<span class="prev change"></span><div id="crab_popup_pane"></div><span class="crab_popup_close"></span><span class="next change"></span>'
                     });
 
                 document.head.appendChild(style);
                 document.body.appendChild(popupBox);
+            },
+            setImg() {
+                const img = data.imgs[data.currentIndex],
+                    box = document.getElementById('crab_popup_box'),
+                    pane = document.getElementById('crab_popup_pane'),
+                    new_Img = utils.createEle('img', {
+                        src: img.src
+                    });
+
+                pane.innerHTML = '';
+                pane.appendChild(new_Img);
+                box.style.display = 'block';
+                box.classList.add('show');
+            },
+            close() {
+                const box = this.box;
+
+                box.classList.remove('show');
+                setTimeout(() => {
+                    box.style.display = 'none';
+                }, 250);
             }
         };
 
@@ -54,16 +95,65 @@
                 }
 
                 if (ele.nodeType !== 1) {
-                    ele = document.querySelector(ele);
+                    ele = utils.slice.call(document.querySelectorAll(ele), 0);
                 }
 
                 this.root = [].concat(ele);
+                this.opts = utils.extends({}, this.defaults, opts);
                 ui.init();
+                this.bindEvent();
+            },
+            bindEvent() {
+                const box = document.getElementById('crab_popup_box'),
+                    close = box.querySelector('.crab_popup_close'),
+                    prev = box.querySelector('.prev'),
+                    next = box.querySelector('.next');
 
+                this.root.forEach((r) => {
+                    const imgs = utils.slice.call(r.querySelectorAll(this.opts.selector), 0);
+
+                    imgs.forEach((img, i, imgs) => {
+                        img.addEventListener('click', () => {
+                            this.setImg(i, imgs);
+                        })
+                    })
+                });
+
+                prev.addEventListener('click', () => {
+                    event.stopPropagation();
+                    this.prev();
+                });
+
+                next.addEventListener('click', () => {
+                    event.stopPropagation();
+                    this.next();
+                });
+
+                close.addEventListener('click', () => {
+                    event.stopPropagation();
+                    ui.close();
+                })
+
+                box.addEventListener('click', () => {
+                    ui.close();
+                });
+            },
+            setImg(i, imgs) {
+                data.setImgs(imgs);
+                data.setImg(i);
+                ui.setImg();
+            },
+            prev() {
+                data.setImg(--data.currentIndex);
+                ui.setImg();
+            },
+            next() {
+                data.setImg(++data.currentIndex);
+                ui.setImg();
             }
         };
 
-        this.init = ctrl.init;
+        this.init = ctrl.init.bind(ctrl);
 
     }
 
